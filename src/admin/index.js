854dd676ff08c2ed88e91fa5a7e67580bab837da
Juti Buican <juti.buican@bottomline.com>
2023-03-07 09:40:19 +0000

    break files into two different ports for admin and clinet

---
+++ src/admin/index.js
@@ -0,0 +1,6 @@
+import application from '../assets/css/application.scss';
+import lib from '../assets/css/lib.css';
+
+export function welcome() {
+  console.log('Welcome to the admin area');
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // const sayHello = require('./greeting.js');
-import { sayHello as greeting } from './greeting';
-// import lib from './assets/css/lib.css';
+import { sayHello as greeting } from './client/greeting';
+import lib from './assets/css/lib.css';
 import './assets/css/application.scss';
 greeting();
